eb6d9c317ba799c10e1ecc0b4c7171477a8f3ef9
zanderz1010 <aafabel0@chicagobooth.edu>
2022-08-10 00:06:38 +0000

    Added Bootstrap

--- db/migrate/20220809231928_create_tasks.rb
+++ db/migrate/20220809231928_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.text :body
-      t.string :status
+      t.string :status, :default => "not_yet_started"
       t.integer :user_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2022_08_09_231928) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "body"
-    t.string "status"
+    t.string "status", default: "not_yet_started"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
